4b2ccb4d081234a63f3c6a105f447de7e7b872b7
Marcelo Marra <marra.marcelo@outlook.com>
2018-07-13 02:22:43 -0300

    site - categorias

---
+++ views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -0,0 +1,1 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/2">imacs - novos</a></li><li><a href="/categories/3">Samsung - notebooks</a></li><li><a href="/categories/4">Samsung - smatphones</a></li>
