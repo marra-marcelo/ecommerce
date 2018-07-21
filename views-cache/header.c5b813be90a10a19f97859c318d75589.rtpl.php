5cd6bf11851286d36382c196b4013fc24c5764ce
Marcelo Marra <marra.marcelo@outlook.com>
2018-07-21 20:43:12 -0300

    Esqueci a senha

--- views-cache/header.c5b813be90a10a19f97859c318d75589.rtpl.php
+++ views-cache/header.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -46,7 +46,7 @@
 
                             <?php if( checkLogin(false) ){ ?>
                              <li><a href="/profile"><i class="fa fa-user"></i> <?php echo getUserName(); ?></a></li>
-                            <li><a href="/logout"><i class="fa fa-closer"></i>Sair</a></li>
+                            <li><a href="/logout"><i class="fa fa-close"></i>Sair</a></li>
                             <?php }else{ ?>
                             <li><a href="/login"><i class="fa fa-lock"></i> Login</a></li>
                             <?php } ?>
