e9226040eabc6508dbce486ac89549593a304a6c
Marcelo Marra <marra.marcelo@outlook.com>
2018-07-25 09:56:56 -0300

    Editar o status do pedido

--- index.php
+++ index.php
@@ -26,6 +26,8 @@ require_once("admin-categories.php");
 
 require_once("admin-products.php");
 
+require_once("admin-orders.php");
+
 $app->run();
 
  ?>
--- vendor/hcodebr/php-classes/src/Model/OrderStatus.php
+++ vendor/hcodebr/php-classes/src/Model/OrderStatus.php
@@ -2,7 +2,7 @@
 
 namespace Hcode\Model;
 
-use \Hacode\DB\Sql;
+use \Hcode\DB\Sql;
 use \Hcode\Model;
 
 
@@ -12,6 +12,13 @@ class OrderStatus extends Model{
 	const AGUARDANDO_PAGAMENTO = 2;
 	const PAGO = 3;
 	const ENTREGUE = 4;
+
+	public static function listAll()
+	{
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_ordersstatus ORDER BY desstatus");
+	}
 
 }
 ?>
